0f31b8d227c29e84cf85dbe2d6cf9a80c19743a4
Thadeu <123416610+Thadeu-Borges@users.noreply.github.com>
2023-02-19 05:00:10 -0300

    Java POO

---
+++ src/aula011/Aula011.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula011;
+
+/**
+ *
+ * @author AAAA
+ */
+public class Aula011 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Pessoa p1 = new Pessoa();
+        Visitante v1 = new Visitante();
+        v1.setNome("Juvenal");
+        v1.setIdade(22);
+        v1.setSexo("M");
+        System.out.println(v1.toString());
+        System.out.println("Juvenal é visitante ");
+
+        System.out.println(" ");
+
+        Aluno a1 = new Aluno();
+        a1.setNome("Cláudio");
+        a1.setMatricula(1111);
+        a1.setCurso("Informática");
+        a1.setIdade(16);
+        a1.setSexo("M");
+        System.out.println(a1.toString());
+        a1.pagarMensalidade();
+
+        System.out.println(" ");
+
+        Bolsista b1 = new Bolsista();
+        b1.setMatricula(1112);
+        b1.setNome("Jubileu");
+        b1.setBolsa(12.5f);
+        b1.setSexo("M");
+        System.out.println(b1.toString());
+        b1.pagarMensalidade();
+    }
+
+}
